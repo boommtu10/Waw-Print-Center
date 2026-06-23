6f825ee7564cbe0f784ab4f254a3bb82c4ed693b
boommtu10 <boommtu10@gmail.com>
2026-06-23 18:02:46 +0700

    config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@
  * 2. นำ URL ที่ได้ (ลงท้ายด้วย /exec) มาวางแทนค่าด้านล่างนี้
  */
 const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbzbio_fptHeb4LEaXJHSUMRdnGcEwVooyfenksPqthX1elHyjwf6CjXLr6htFyvlCBt/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbw7Fdl_gYixV8FaD2lPXc0_0jTyHO03G7Vm1tWLLOn7mRuCr8njUJLfZoIu7YbytvK3/exec',
   STORE_NAME: 'วาว Print Center',
 };
